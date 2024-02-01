fdea7f271b3af2fcad502fa2f0851757c1d9917b
Meenaz Momin <meenaz.riyaz@apsissolutions.com>
2024-02-01 17:10:04 +0530

    added subject and body for verification email

--- backend/accounts/signals.py
+++ backend/accounts/signals.py
@@ -3,8 +3,8 @@ from portal.services import trigger_mails
 
 def send_verification_email(sender, instance, created, **kwargs):
     if created:
-        subject = "Your One-Time Password for Dakhni Recipe App"
-        body = f"""Dear {instance.name},\n\nThank you for using Dakhni Recipe app."""
+        subject = "Welcome to Dakhni Recipe!"
+        body = f"""Dear {instance.name},\n\nCongratulations! You've successfully signed up for Dakhni Recipe, your go-to mobile app for delightful culinary experiences.\n\nGet ready to embark on a flavorful journey with our app, where you'll discover a rich collection of authentic Dakhni recipes to spice up your kitchen. From aromatic biryanis to mouthwatering curries, we've got it all!\n\nHappy cooking!\n\nBest regards,\n\nThe Dakhni Recipe Team"""
 
         recievers = [instance.email]
         to_cc = ["momin.meenaz29@gmail.com"]
